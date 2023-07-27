c9ec9ac88e8fb845eded115fc9a56485f669d42e
Zhenxu Wang <mason123wp@gmail.com>
2023-07-27 03:11:00 -0400

    implement http status codes

---
+++ src/main/java/constants/HttpStatus.java
@@ -0,0 +1,10 @@
+package constants;
+
+public class HttpStatus {
+	
+	public static final int OK = 200;
+	public static final int BAD_REQUEST = 400;
+	public static final int NOT_FOUND = 404;
+	public static final int INTERNAL_SERVER_ERROR = 500;
+	
+}
