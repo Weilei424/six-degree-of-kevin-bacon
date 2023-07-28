b21f3454af1e859b9d42e01b40486b49a68bd5b9
Zhenxu Wang <mason123wp@gmail.com>
2023-07-27 22:21:44 -0400

    implement fields, constructors, getters and setters

--- src/main/java/pojo/Actor.java
+++ src/main/java/pojo/Actor.java
@@ -1,5 +1,32 @@
 package pojo;
 
 public class Actor {
+	private Long id;
+	private String name;
+	
+	public Actor() {
+		super();
+	}
 
+	public Actor(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 }
--- src/main/java/pojo/Movie.java
+++ src/main/java/pojo/Movie.java
@@ -1,5 +1,31 @@
 package pojo;
 
 public class Movie {
+	private Long id;
+	private String name;
+	
+	public Movie() {
+		super();
+	}
 
+	public Movie(Long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 }
