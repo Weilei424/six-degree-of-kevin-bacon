0dce19341b1d94f1ba407a4bd2e19b5947ad7a0f
Zhenxu Wang <mason123wp@gmail.com>
2023-07-28 20:13:19 -0400

    add InvalidRequestException.java for 400

---
+++ src/main/java/exceptions/InvalidRequestException.java
@@ -0,0 +1,12 @@
+package exceptions;
+
+public class InvalidRequestException extends RuntimeException {
+
+	public InvalidRequestException() {
+		super();
+	}
+
+	public InvalidRequestException(String message) {
+		super(message);
+	}
+}
