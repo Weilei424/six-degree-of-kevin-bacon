62e5849c96a10281fd16cf12d28635f6e0960e22
Zhenxu Wang <mason123wp@gmail.com>
2023-07-28 20:14:46 -0400

    add EntityNotFoundException.java for 404

---
+++ src/main/java/exceptions/EntityNotFoundException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+public class EntityNotFoundException extends Exception {
+
+	public EntityNotFoundException() {
+		super();
+	}
+
+	public EntityNotFoundException(String message) {
+		super(message);
+	}
+
+}
