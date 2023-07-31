280e241a06a40eaf8ea977d886497719d8950204
Zhenxu Wang <mason123wp@gmail.com>
2023-07-31 03:28:17 -0400

    implement Neo4jBooks.addNode, Neo4jBooks.getNode

--- src/main/java/persistence/Neo4jBooks.java
+++ src/main/java/persistence/Neo4jBooks.java
@@ -1,9 +1,18 @@
 package persistence;
 
+import java.security.Policy.Parameters;
+
 import org.neo4j.driver.v1.AuthTokens;
 import org.neo4j.driver.v1.Config;
 import org.neo4j.driver.v1.Driver;
 import org.neo4j.driver.v1.GraphDatabase;
+import org.neo4j.driver.v1.Session;
+import org.neo4j.driver.v1.StatementResult;
+import org.neo4j.driver.v1.Transaction;
+
+import pojo.Actor;
+import pojo.Movie;
+import exceptions.InvalidRequestException;
 
 public class Neo4jBooks {
 	
@@ -14,7 +23,7 @@ public class Neo4jBooks {
 	private Neo4jBooks() {
 		uri = "bolt://localhost:7687";
 		Config config = Config.build().withoutEncryption().build();
-		driver = GraphDatabase.driver(uri, AuthTokens.basic("neo4j", "1234"), config);
+		driver = GraphDatabase.driver(uri, AuthTokens.basic("neo4j", "12345678"), config);
 	}
 	
 	public static Neo4jBooks getInstance() {
@@ -22,5 +31,55 @@ public class Neo4jBooks {
 			instance = new Neo4jBooks();
 		}
 		return instance;
+	}
+	
+	public <T> void addNode(String id, String name, Class<T> c) {
+		
+		try (Session session = driver.session()) {
+			String label = "";
+			if (c == Movie.class) {
+				label = "m: movie";
+			} else if (c == Actor.class) {
+				label = "a: actor";
+			} else {
+				throw new InvalidRequestException();
+			}
+			String query = String.format("CREATE (:%s {id:", label);
+            session.writeTransaction(tx -> tx.run(
+            		query
+            		+ id
+            		+ "\", name:\""
+            		+ name
+            		+ "\"})"));
+//            session.writeTransaction(tx -> tx.run(
+//            		), parameters());
+		}
+	}
+	
+	public <T> StatementResult getNode(String id, Class<T> c) {
+		
+		try (Session session = driver.session()) {
+			try (Transaction tx = session.beginTransaction()) {
+				String label = "";
+				if (c == Movie.class) {
+					label = "x: movie";
+				} else if (c == Actor.class) {
+					label = "x: actor";
+				} else {
+					throw new InvalidRequestException();
+				}
+				StatementResult sr = tx.run(
+						"MATCH ("
+						+ label
+						+ ")\nWHERE x.id"
+						+ " = \""
+						+ id 
+						+ "\"\n"
+						+ "RETURN x;\n"
+						);
+				return sr;
+			}
+		}
+		
 	}
 }
