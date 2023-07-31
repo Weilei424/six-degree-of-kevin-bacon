24b5f494695bd8da5e77266b5dd65ab7c6f8fda0
Zhenxu Wang <mason123wp@gmail.com>
2023-07-31 04:33:08 -0400

    implement getMovie and static factory

--- src/main/java/service/MovieServiceImpl.java
+++ src/main/java/service/MovieServiceImpl.java
@@ -4,12 +4,27 @@ import org.json.JSONObject;
 
 import exceptions.EntityNotFoundException;
 import persistence.MovieDAO;
+import persistence.MovieDAOImpl;
 import persistence.MovieStub;
 import pojo.Movie;
 
 public class MovieServiceImpl implements MovieService {
 
-	private MovieDAO movieDAO = new MovieStub(); // Using stub db before we have actual db setup
+	private MovieDAO stub = new MovieStub(); // Using stub db before we have actual db setup
+	private static MovieServiceImpl instance;
+	private MovieDAO movieDAO;
+	
+	private MovieServiceImpl() {
+		movieDAO = MovieDAOImpl.getInstance();
+	}
+	
+	public static MovieServiceImpl getInstance() {
+		if (instance == null) {
+			instance = new MovieServiceImpl();
+		}
+		
+		return instance;
+	}
 
 	@Override
 	public void addMovie(JSONObject jsonObject) {
@@ -20,6 +35,6 @@ public class MovieServiceImpl implements MovieService {
 	@Override
 	public JSONObject getMovie(String query) throws EntityNotFoundException {
 		
-		return new JSONObject(movieDAO.getMovie(query));
+		return new JSONObject(stub.getMovie(query));
 	}
 }
