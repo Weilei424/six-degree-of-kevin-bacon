b253aff83bd04b70af993687141a3b9c51aac79a
Zhenxu Wang <mason123wp@gmail.com>
2023-07-31 04:33:22 -0400

    implement static factory

--- src/main/java/persistence/ActorDAOImpl.java
+++ src/main/java/persistence/ActorDAOImpl.java
@@ -3,8 +3,23 @@ package persistence;
 import pojo.Actor;
 
 public class ActorDAOImpl implements ActorDAO {
-	private ActorStub db;
+	
+	private static ActorDAOImpl instance;
+	private Neo4jBooks nb;
+	private ActorStub stub;
 
+	private ActorDAOImpl() {
+		nb = Neo4jBooks.getInstance();
+	}
+	
+	public static ActorDAOImpl getInstance() {
+		if (instance == null) {
+			instance = new ActorDAOImpl();
+		}
+		
+		return instance;
+	}
+	
 	@Override
 	public Actor getActor(String query) {
 		// TODO Auto-generated method stub
--- src/main/java/persistence/MovieDAOImpl.java
+++ src/main/java/persistence/MovieDAOImpl.java
@@ -1,13 +1,41 @@
 package persistence;
 
+import org.neo4j.driver.v1.Record;
+import org.neo4j.driver.v1.StatementResult;
+
+import exceptions.InvalidRequestException;
 import pojo.Movie;
 
 public class MovieDAOImpl implements MovieDAO {
-	private MovieStub db;
-
+	
+	private static MovieDAOImpl instance;
+	private Neo4jBooks nb;
+	
+	private MovieDAOImpl() {
+		nb = Neo4jBooks.getInstance();
+	}
+	
+	public static MovieDAOImpl getInstance() {
+		if (instance == null) {
+			instance = new MovieDAOImpl();
+		}
+		
+		return instance;
+	}
+	
 	@Override
 	public Movie getMovie(String query) {
-		// TODO Auto-generated method stub
-		return null;
+		String id = query.split("=")[1];
+		StatementResult sr = nb.getNode(id, Movie.class);
+		Movie movie = new Movie();
+		
+		if (sr.hasNext()) {
+			Record r = sr.next();
+			movie.setId(r.get("id").asString());
+			movie.setName(r.get("name").asString());
+			return movie;
+		} else {
+			throw new InvalidRequestException();
+		}
 	}
 }
