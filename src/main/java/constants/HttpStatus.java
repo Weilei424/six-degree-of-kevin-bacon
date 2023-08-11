81418f91688a9338e7151e5f46d602e34c976f76
Zhenxu Wang <mason123wp@gmail.com>
2023-08-11 05:13:58 -0400

    add field NO_CONTENT

--- src/main/java/constants/HttpStatus.java
+++ src/main/java/constants/HttpStatus.java
@@ -6,5 +6,5 @@ public class HttpStatus {
 	public static final int BAD_REQUEST = 400;
 	public static final int NOT_FOUND = 404;
 	public static final int INTERNAL_SERVER_ERROR = 500;
-	
+	public static final int NO_CONTENT = 204;
 }
